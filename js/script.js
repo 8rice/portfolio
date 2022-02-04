2aae7ced564958af9d32b3bd6b4fe1e867564492
8rice <bricesabuco@gmail.com>
2022-02-04 13:19:50 +0100

    fix cursor offsetHeight

--- js/script.js
+++ js/script.js
@@ -9,19 +9,21 @@ let centerCursor = 30
 
 document.onmousemove = handleMouseMove;
 
+console.log(firstPageProjectLeft.offsetTop + firstPageProjectLeft.offsetHeight)
+
 function handleMouseMove(event) {
     mouseX = event.pageX - centerCursor //30 pour center sur le cursor
     mouseY = event.pageY - centerCursor
 
     if (mouseX > window.innerWidth / 2 + 10) { // 15 pour ne pas avoir de bug au moment du passage de l'un a l'autre
-        if (mouseY > firstPageProjectLeft.offsetTop && mouseY < firstPageProjectLeft.offsetTop + window.innerHeight) {
+        if (mouseY > firstPageProjectLeft.offsetTop && mouseY < firstPageProjectLeft.offsetTop + firstPageProjectLeft.offsetHeight - 50) {
             firstPageProjectLeft.style.backgroundColor = 'white'
             firstPageProjectRight.style.backgroundColor = 'aliceblue'
             span.style.width = '100px'
             span.style.height = '100px'
             centerCursor = 50
         }
-        else if (mouseY < firstPageProjectLeft.offsetTop - 10) {
+        else if (mouseY < firstPageProjectLeft.offsetTop - 10 || mouseY > firstPageProjectLeft.offsetTop + firstPageProjectLeft.offsetHeight - 50) {
             span.style.width = '60px'
             span.style.height = '60px'
             centerCursor = 30
